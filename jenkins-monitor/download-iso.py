abbacd43e7089b0ddca6588e3776e04d7a705ee8
Nick Wang <nwang@suse.com>
2015-08-25 10:14:50 +0800

    Add debug log and exception for occasionally fail to connect download.suse.de.

--- jenkins-monitor/download-iso.py
+++ jenkins-monitor/download-iso.py
@@ -26,15 +26,26 @@ PRODUCTS = { "sle": {"pattern": sle_pattern, "database": sle_db, "verify": postf
              "sleha": {"pattern": sleha_pattern, "database": sleha_db, "verify": postfix_sha,
                      "process": "", "iso":"", "pipe": ""} }
 
+Enable_debug = False
+
+def DEBUG(string):
+	if Enable_debug:
+		print(string)
+	else:
+		pass
+
 def verifySha256(name):
 	get_sha = os.popen("/usr/bin/sha256sum ./ISO/%s" % name)
 	shaNo = get_sha.readline().split()[0]
 	if os.system("grep %s ./ISO/%s >/dev/null 2>&1" % (shaNo, name+postfix_sha)) >> 8 == 0:
+		DEBUG("Succeed to verify sha256 number.")
 		return True
 	else:
+		DEBUG("Downloaded ISO is incomplete.")
 		return False
 
 def updateRecord(pname, build):
+	DEBUG("Record update. product: %s, build: %s" % (pname, build))
 	fd = open(PRODUCTS[pname]["database"], "w")
 	fd.write(build)
 	fd.close()
@@ -42,8 +53,9 @@ def updateRecord(pname, build):
 def downloadFiles(pname, nbuild, conn):
 	name = PRODUCTS[pname]["pattern"] % nbuild
 
-	print "Process %d - Start to download: %s." % (PRODUCTS[pname]["process"].pid, URL+name+postfix_sha,)
+	print "Process %d - Start to download: %s." % (PRODUCTS[pname]["process"].pid, URL+name+postfix_sha)
 	urllib.urlretrieve(URL+name+postfix_sha, "./ISO/"+name+postfix_sha)
+
 	print "Process %d - Start to download: %s." % (PRODUCTS[pname]["process"].pid, URL+name)
 	urllib.urlretrieve(URL+name, "./ISO/"+name)
 
@@ -53,6 +65,7 @@ def downloadFiles(pname, nbuild, conn):
 		return False
 	else:
 		#Save new downloaded ISO's name
+		DEBUG("ISO %s downloaded." % name)
 		conn.send(name)
 		conn.close()
 
@@ -60,7 +73,15 @@ def downloadFiles(pname, nbuild, conn):
 		return True
 
 def hasNew(pname, old):
-	urlfd = urllib.urlopen(URL)
+	#Occasionally fail to connect download.suse.de
+	while True:
+		try:
+			urlfd = urllib.urlopen(URL)
+		except IOError, e:
+			print "urlopen: %s.\nURL is %s." % (e.__str__(), URL)
+			time.sleep(20)
+		else:
+			break
 
 	for line in urlfd.readlines():
 		result = re.search(PRODUCTS[pname]["pattern"] % "([0-9]+)", line)
@@ -69,6 +90,7 @@ def hasNew(pname, old):
 			break
 
 	if int(build) > int(old):
+		DEBUG("Found new build %s for product %s." % (build, pname))
 		return build
 	else:
 		return ""
@@ -106,11 +128,14 @@ def mountISO(pname):
 	else:
 		mount_info = os.popen("mount |grep %s" % mount_dir).readline()
 		if mount_info != "":
+			DEBUG("Old mount info: %s" % mount_info)
 			obsolete_iso = mount_info.split()[0]
 			os.system("umount -d %s >/dev/null 2>&1" % mount_dir)
 
 	if os.system("mount -o loop %s %s" % (iso_dir+PRODUCTS[pname]["iso"], mount_dir)) >> 8 == 0:
+		DEBUG("Succeed to mount new ISO.")
 		if obsolete_iso != "":
+			DEBUG("Remove obsolete ISO.")
 			os.system("rm -rf %s* >/dev/null 2>&1" % obsolete_iso)
 		return True
 	else:
@@ -138,6 +163,7 @@ def main():
 			break
 		else:
 			time.sleep(pause_time)
+			DEBUG("Checking new build...")
 
 	print "Monitor process End."
 
