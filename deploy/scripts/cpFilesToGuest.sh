72b241a3394c75dd6d1044a06fea94b1296663cb
Nick Wang <nwang@suse.com>
2015-09-21 16:49:54 +0800

    Support custom cluster_conf location.

--- deploy/scripts/cpFilesToGuest.sh
+++ deploy/scripts/cpFilesToGuest.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 function usage()
 {
-  echo "cpFilesToGuest.sh <WORKING_DIR_IN_GUEST>"
+  echo "cpFilesToGuest.sh <BUILD_DIR_IN_HOST> <WORKING_DIR_IN_GUEST>"
   exit
 }
 
-CLUSTER_DIR=$1
+BUILD_DIR=$1
+CLUSTER_DIR=$2
 
 for ip in `cat ../cluster_conf |grep IP_NODE |cut -d "=" -f 2`
 do
@@ -13,7 +14,7 @@ do
 ssh root@${ip} "mkdir -p ${CLUSTER_DIR}/templete; mkdir -p ${CLUSTER_DIR}/scripts"
 
 scp ./configCluster.sh ./functions root@${ip}:${CLUSTER_DIR}/scripts/
-scp ../cluster_conf root@${ip}:${CLUSTER_DIR}
+scp ${BUILD_DIR}/cluster_conf root@${ip}:${CLUSTER_DIR}
 scp ../templete/*_templete root@${ip}:${CLUSTER_DIR}/templete/
 
 ssh root@${ip} "cd ${CLUSTER_DIR}; ${CLUSTER_DIR}/scripts/configCluster.sh "
