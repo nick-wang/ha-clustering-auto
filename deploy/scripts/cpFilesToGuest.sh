90bcb4619f3c2ad1cbe881f5b96d2559c8e38187
Nick Wang <nwang@suse.com>
2015-09-21 16:58:09 +0800

    Using file instead of directory.

--- deploy/scripts/cpFilesToGuest.sh
+++ deploy/scripts/cpFilesToGuest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 function usage()
 {
-  echo "cpFilesToGuest.sh <BUILD_DIR_IN_HOST> <WORKING_DIR_IN_GUEST>"
+  echo "cpFilesToGuest.sh <CLUSTER_CONF_IN_HOST> <WORKING_DIR_IN_GUEST>"
   exit
 }
 
-BUILD_DIR=$1
+CLUSTER_CONF=$1
 CLUSTER_DIR=$2
 
 for ip in `cat ../cluster_conf |grep IP_NODE |cut -d "=" -f 2`
@@ -14,7 +14,7 @@ do
 ssh root@${ip} "mkdir -p ${CLUSTER_DIR}/templete; mkdir -p ${CLUSTER_DIR}/scripts"
 
 scp ./configCluster.sh ./functions root@${ip}:${CLUSTER_DIR}/scripts/
-scp ${BUILD_DIR}/cluster_conf root@${ip}:${CLUSTER_DIR}
+scp ${CLUSTER_CONF} root@${ip}:${CLUSTER_DIR}
 scp ../templete/*_templete root@${ip}:${CLUSTER_DIR}/templete/
 
 ssh root@${ip} "cd ${CLUSTER_DIR}; ${CLUSTER_DIR}/scripts/configCluster.sh "
