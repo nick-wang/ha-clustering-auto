8e3d8ac9a8275aec5f4db75f8a8747e9663bd147
Nick Wang <nwang@suse.com>
2015-09-21 11:34:08 +0800

    Add scripts to cp necessary files to guest.

---
+++ deploy/scripts/cpFilesToGuest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+function usage()
+{
+  echo "cpFilesToGuest.sh <WORKING_DIR_IN_GUEST>"
+  exit
+}
+
+CLUSTER_DIR=$1
+
+for ip in `cat ../cluster_conf |grep IP_NODE |cut -d "=" -f 2`
+do
+{
+ssh root@${ip} "mkdir -p ${CLUSTER_DIR}/templete; mkdir -p ${CLUSTER_DIR}/scripts"
+
+scp configCluster.sh root@${ip}:${CLUSTER_DIR}/scripts
+scp ../templete/cluster_conf root@${ip}:${CLUSTER_DIR}
+scp ../templete/*_templete root@${ip}:${CLUSTER_DIR}/templete/
+
+ssh root@${ip} "cd ${CLUSTER_DIR}; ${CLUSTER_DIR}/scripts/configCluster.sh "
+} &
+done
+
+# Wait all sub process to finish
+wait
