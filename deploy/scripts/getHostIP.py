7941de61c55009e1e1c2634a44fcf0af4dd23c34
Nick Wang <nwang@suse.com>
2015-09-23 13:44:27 +0800

    Enable vir net device to get_ip_list.

--- deploy/scripts/getHostIP.py
+++ deploy/scripts/getHostIP.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#!/usr/bin/python
 import sys
 import os
 import socket
@@ -13,6 +13,7 @@ def get_ipaddr_by_interface(interface):
     ifname = interface.strip()
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
+        #usr/include/linux/sockios.h
         ipaddr = socket.inet_ntoa(fcntl.ioctl(s.fileno(), \
                                   0x8915, \
                                   struct.pack('256s', ifname[:15]))[20:24])
@@ -31,7 +32,7 @@ def get_ip_list():
     interfaces = get_interfaces()
     for interface in interfaces:
         interface = interface.strip()
-        if interface_filter(interface, 'vir') or interface_filter(interface, 'vnet'):
+        if interface_filter(interface, 'vnet'):
             continue
         ipaddr = get_ipaddr_by_interface(interface)
 
@@ -53,14 +54,14 @@ def get_net_mask(interface):
 def get_netaddr(interface):
     netmask = get_net_mask(interface).split('.')
     ipaddr = get_ipaddr_by_interface(interface).split('.')
-#    if netmask.strip() == '' or ipaddr.strip() == '':
-#        return ''
+
     if len(netmask) != 4 or len(ipaddr) != 4:
         return ''
-    return "%d.%d.%d.%d" %(((int(ipaddr[0]))&(int(netmask[0]))), \
-                           ((int(ipaddr[1]))&(int(netmask[1]))), \
-                           ((int(ipaddr[2]))&(int(netmask[2]))), \
-                           ((int(ipaddr[3]))&(int(netmask[3]))))
+
+    return "%d.%d.%d.%d" % (int(ipaddr[0]) & int(netmask[0]), \
+                            int(ipaddr[1]) & int(netmask[1]), \
+                            int(ipaddr[2]) & int(netmask[2]), \
+                            int(ipaddr[3]) & int(netmask[3]))
 
 
 if __name__ == "__main__":
