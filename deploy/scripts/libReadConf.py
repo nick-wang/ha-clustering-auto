f49a2f874aefe31e2403ab87806a201103bba2af
Nick Wang <nwang@suse.com>
2015-10-09 18:29:54 +0800

    Add gitingore file.

---
+++ deploy/scripts/libReadConf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+
+def readClusterConf(conf):
+    fd = file(conf, "r")
+    lines = [ x.strip() for x in fd.readlines() ]
+    fd.close()
+
+    temp = {}
+    for line in lines:
+        temp[line.split("=")[0]] = line.split("=")[1]
+
+    return temp
+
+def test(conf):
+    print(readClusterConf(conf))
+
+if __name__ == "__main__":
+    '''
+        This is a library to read cluster config file.
+    '''
+    test(sys.argv[1])
