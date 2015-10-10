7fc0211136cd198397ecefcbebd7f3dd05710d7a
Nick Wang <nwang@suse.com>
2015-10-10 12:54:29 +0800

    Add one library to convert the result of testcase to junit xml for jenkins.

---
+++ deploy/TestReport/libJunitXml.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env
+
+from junit_xml import TestSuite, TestCase
+
+def assertCase(testcase, func):
+    result = func
+    if not result["pass"]:
+        testcase.add_error_info(result["message"], result['output'])
+
+def skipCase(testcase, message, output):
+    testcase.add_skipped_info(message, output)
+
+def _exampleFunc(temp):
+    result = {"pass":False, "message":"", "output":""}
+
+    #Own test steps
+
+    #Test OK
+    if temp:
+        result["pass"] = True
+        return result
+    #Test NG
+    else:
+        result["message"] = "Test failed!"
+        result["output"] = "No more space in sda."
+        return result
+
+def _test():
+    test_case1 = TestCase('Testname1', 'SetupCluster.Name')
+    test_case2 = TestCase('Testname2', 'SetupCluster.Name')
+    test_case3 = TestCase('Testname3', 'SetupCluster.Misc')
+    test_case4 = TestCase('Testname4', 'SetupCluster.Misc')
+
+    test_cases = [test_case1, test_case2, test_case3, test_case4]
+    ts = TestSuite("My Test Suite", test_cases)
+
+    #Run and verify test case
+    assertCase(test_case1, _exampleFunc(True))
+    assertCase(test_case2, _exampleFunc(False))
+    assertCase(test_case3, _exampleFunc(True))
+    #Skip test case
+    skipCase(test_case4, "Skip Testname4.", "Testname2 is failed.")
+
+    print(ts.to_xml_string([ts]))
+
+    #with open('output.xml') as f:
+    #    ts.write_to_file(f)
+
+if __name__ == "__main__":
+    '''
+        This is a library to test and output in junit xml for jenkins.
+    '''
+    _test()
