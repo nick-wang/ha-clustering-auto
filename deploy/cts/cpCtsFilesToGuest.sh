811f372f891eff8c24136fa9fc729a5eb91d876d
bin.liu <bliu@suse.com>
2015-10-13 17:48:52 +0800

    modify deploy/cts/cpCtsFilesToGuest.sh to fix bug that failed to copy logfile from test driver node to jenkins node

--- deploy/cts/cpCtsFilesToGuest.sh
+++ deploy/cts/cpCtsFilesToGuest.sh
@@ -24,8 +24,9 @@ scp ../cts/configAndRunCts.sh ../scripts/functions root@${ip}:${CTS_DIR}/scripts
 scp ${CTS_CONF} ${CLUSTER_CONF} root@${ip}:${CTS_DIR}
 
 ssh root@${ip} "chmod 0600 /root/.ssh/id_rsa;cd ${CTS_DIR}; ${CTS_DIR}/scripts/configAndRunCts.sh "
-scp root@${ip}:/tmp/cts-configuration/my.log .
 } &
 done
-
+source $CLUSTER_CONF
+scp root@${IP_NODE1}:/tmp/cts-configuration/my.log $4
+echo "scp root@${IP_NODE1}:/tmp/cts-configuration/my.log $4"
 wait
