4de671dc39f03cd512910f2f316167b5b5481588
bin.liu <bliu@suse.com>
2015-10-14 17:17:21 +0800

    add hb_report and collect it to workspace

--- deploy/cts/cpCtsFilesToGuest.sh
+++ deploy/cts/cpCtsFilesToGuest.sh
@@ -26,7 +26,18 @@ scp ${CTS_CONF} ${CLUSTER_CONF} root@${ip}:${CTS_DIR}
 ssh root@${ip} "chmod 0600 /root/.ssh/id_rsa;cd ${CTS_DIR}; ${CTS_DIR}/scripts/configAndRunCts.sh "
 } &
 done
+wait
+
 #source $CLUSTER_CONF
 #scp root@${IP_NODE1}:/tmp/cts-configuration/my.log $4
-echo "scp root@${IP_NODE1}:/tmp/cts-configuration/my.log $4"
-wait
+#echo "scp root@${IP_NODE1}:/tmp/cts-configuration/my.log $4"
+
+for ip in `cat $CLUSTER_CONF |grep IP_NODE |cut -d "=" -f 2`
+do
+{
+	date_str=`date +%s`
+	hb_report="hb_report_${ip}_${date_str}"
+	ssh root@${ip} "systemctl start pacemaker; hb_report -f 0:00 $hb_report"
+	scp ${ip}:/root/$hb_report* $4
+}
+done
