95f326743781e5c115f25e653d485713af5391b3
Nick Wang <nwang@suse.com>
2016-05-09 16:24:01 +0800

    Modify howto of drbd.

---
+++ deploy/scripts/drbd/crmDRBD.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Import ENV conf
+. cluster_conf
+. scripts/functions
+
+#Configure crm resources
+#Echo should configure crm resources
+isMaster "$HOSTNAME_NODE1"
+if [ $? -eq 0 ]
+then
+  echo "Before configuring CRM"
+  logit crm configure show
+  for resname in `drbdadm dump all |grep -P "^resource .+ {" | cut -d " " -f 2`
+  do
+    crm configure primitive res-${resname} ocf:linbit:drbd \
+                  params drbd_resource=${resname}
+  done
+  logit crm configure show
+  echo "After configuring CRM"
+fi
+  
+sleep 5
+echo "reboot"
+reboot
