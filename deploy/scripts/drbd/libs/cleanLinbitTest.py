cd6579913fdbecca47832e9ea8ed76c37702cb1f
Nick Wang <nwang@suse.com>
2016-08-30 16:27:26 +0800

    Add cleanLinbitTest.py to cleanup the drbd setup and lvm for drbd-test.

---
+++ deploy/scripts/drbd/libs/cleanLinbitTest.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python2
+
+import os, subprocess
+import time
+
+vg_name = "scratch"
+
+def remove_lvm():
+    lvs = [ line.strip() for line in 
+        os.popen('lvs |grep %s' % vg_name) ]
+
+    for lv in lvs:
+        lv_name = lv.split()[0]
+        os.popen("lvremove -f /dev/scratch/%s" % lv_name)
+
+def check_res_exist(show=False):
+    res = [ line.strip() for line in
+        os.popen('drbdsetup status |grep "^\w" |cut -d " " -f 1').readlines() ]
+
+    if len(res) != 0:
+        if show == True:
+            print "Resource existed!!!"
+            print "Get the resource name: %s" % res
+    else:
+        if show == True:
+            print "No resource defined."
+            print "Remove lvs of scratch."
+        return None
+   
+    # If multiple res exist, previous test case is not clean
+    return res
+
+def show_status():
+    # subprocess.call will show output to screen
+    # subprocess.check_output will show nothing
+    tmp = subprocess.call( [ "drbdsetup", "status" ] )
+
+def down_res(res):
+    tmp_num = os.popen( "drbdsetup show %s |grep -c connection" %
+        res ).readline().strip()
+    node_num = int(tmp_num) + 1
+
+    # id:0 is node itself
+    for peer_id in range(1, node_num):
+        os.popen( "drbdsetup del-peer %s %d" % (res, peer_id) )
+
+    time.sleep(2)
+    subprocess.call( [ "drbdsetup", "down", res ] )
+
+def main():
+    res=check_res_exist()
+
+    #show_status()
+    if res is not None:
+        for resource in res:
+            down_res(resource)
+    #show_status()
+
+    remove_lvm()
+
+if __name__ == "__main__":
+    main()
