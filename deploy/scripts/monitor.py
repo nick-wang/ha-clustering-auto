8a8dbd87d9bd1f08a087d4f5602a267ecb277dcd
Bin Liu <bliu@suse.com>
2016-12-08 17:30:56 +0800

    monitor whether a package changed to triger jekins build

--- deploy/scripts/monitor.py
+++ deploy/scripts/monitor.py
@@ -1,9 +1,12 @@
+#! /usr/bin/python
 #http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/GA/standard/x86_64/
+
 import re, os, urllib2, sys
 #pacemaker-1.1.15-17-x86_64.rpm
 #corosync-2.3.5-4.2.x86_64.rpm
 #patterns=['pacemaker-(\d)','corosync-(\d)+']
 patterns=['^pacemaker-(\d)+\.(\d)+\.(\d)+-(\d)+\.(\d)+\.(\w)+\.rpm','^corosync-(\d)+\.(\d)+\.(\d)+-(\d)+\.(\d)+\.\w+\.rpm']
+packages_monitoring=['pacemaker', 'corosync']
 
 def isTheRPM(package):
     if not package.endswith('rpm'):
@@ -18,10 +21,11 @@ def get_all_packages(html):
     content = html.split()
     packages = []
     for line in content:
-        if "pacemaker" in line or "corosync" in line:
-            package=line.split('"')[1].split('"')[0]
-            if isTheRPM(package) and package not in packages:
-                packages.append(package)
+        for package in packages_monitoring:
+            if package in line:
+                package=line.split('"')[1].split('"')[0]
+                if isTheRPM(package) and package not in packages:
+                    packages.append(package)
     return packages
 
 def save_package_info(html):
@@ -39,6 +43,17 @@ def get_package_info():
         return f.read().split()
     return None
 
+def need_update(old_packages, new_packages):
+    need_updated = {}
+    for old_package in old_packages:
+        for new_package in new_packages:
+            if new_package == old_package:
+                continue
+            if old_package.split('-')[0] not in new_package:
+                continue
+            need_updated[old_package] = new_package
+    return need_updated
+
 def main(url):
     #response=urllib2.urlopen('http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/GA/standard/x86_64/')
     try:
@@ -48,16 +63,32 @@ def main(url):
         print "wrong url %s" % url
         print e
         sys.exit(-1)
-
     old_packages = get_package_info()
     new_packages = get_all_packages(html)
-    if old_packages is None or old_packages != new_packages:
-        print "please update"
+    need_updated = {}
+
+    if old_packages is None:
+        print '-----------------------'
+        print "please update packages:"
+        for new_package in new_packages:
+            print new_package
+        print '-----------------------'
         save_package_info(html)
         sys.exit(0)
-    else:
-        print "do not update"
-        sys.exit(1)
+
+    elif len(new_packages) > 0:
+        need_updated = need_update(old_packages, new_packages)
+        if len(need_updated) > 0:
+            print "-----------------------"
+            print "please update packages:"
+            for old_package in need_updated.keys():
+                print "update %s to %s" %(old_package, need_updated[old_package])
+            print "-----------------------"
+            save_package_info(html)
+            sys.exit(0)
+
+    print "do not update"
+    sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
