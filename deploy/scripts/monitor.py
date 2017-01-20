392a12bef2c4dae26b942f0bff6784c4b7e56c44
Nick Wang <nwang@suse.com>
2017-01-20 14:32:53 +0800

    Support monitor as module and add a unified pattern.

--- deploy/scripts/monitor.py
+++ deploy/scripts/monitor.py
@@ -3,19 +3,30 @@
 
 import re, os, urllib2, sys
 import getopt
-#pacemaker-1.1.15-17-x86_64.rpm
-#corosync-2.3.5-4.2.x86_64.rpm
-#patterns=['pacemaker-(\d)','corosync-(\d)+']
-patterns=['^pacemaker-(\d)+\.(\d)+\.(\d)+-(\d)+\.(\d)+\.(\w)+\.rpm','^corosync-(\d)+\.(\d)+\.(\d)+-(\d)+\.(\d)+\.\w+\.rpm']
-packages_monitoring=['pacemaker', 'corosync']
-proj_name=""
+support_schemas = {'pacemaker':
+                       ('pacemaker', 'corosync'),
+                   'drbd':
+                       ('drbd', 'drbd-utils', 'kernel-default'),
+                   'cluster-md':
+                       ('mdadm', 'cluster-md-kmp-default', 'kernel-default'),
+                  }
+
+module = 'pacemaker'
+packages_monitoring = ''
+proj_name = ""
+dummy_dir = "/tmp/jenkins-dummy/pkgs-change/"
 
 def isTheRPM(package):
+    #version=re.groups()[0]
+    #release=tmp.groups()[2][0:-1]
+    #arch=tmp.groups()[-1]
     if not package.endswith('rpm'):
         return False
-    for pattern in patterns:
-        p = re.compile(pattern)
-        if p.match(package):
+    pattern="%s-(\d+(\.[\w\+]+)+)-((\d+\.)+)(\w+)\.rpm"
+
+    for pkg in packages_monitoring:
+        pa = re.compile(pattern % pkg)
+        if pa.match(package):
             return True
     return False
 
@@ -59,6 +70,7 @@ def need_update(old_packages, new_packages):
     return need_updated
 
 def main(url):
+    global packages_monitoring
     #response=urllib2.urlopen('http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/GA/standard/x86_64/')
     try:
         response=urllib2.urlopen(url)
@@ -67,13 +79,16 @@ def main(url):
         print "wrong url %s" % url
         print e
         sys.exit(-1)
+    if packages_monitoring == '':
+        packages_monitoring = support_schemas[module]
+
     old_packages = get_package_info()
     new_packages = get_all_packages(html)
     need_updated = {}
 
     if old_packages is None:
         print '-----------------------'
-        print "please update packages:"
+        print "Initializing the packages:"
         for new_package in new_packages:
             print new_package
         print '-----------------------'
@@ -86,7 +101,11 @@ def main(url):
             print "-----------------------"
             print "please update packages:"
             for old_package in need_updated.keys():
-                print "update %s to %s" %(old_package, need_updated[old_package])
+                if old_package != "NEW":
+                    print "update %s to %s" %(old_package, need_updated[old_package])
+                else:
+                    for new_record in need_updated[old_package]:
+                        print "new record %s" % new_record
             print "-----------------------"
             save_package_info(html)
             sys.exit(0)
@@ -95,13 +114,13 @@ def main(url):
     sys.exit(1)
 
 def usage():
-    print "./monitor.py -u <url> -p <packages> -P <project_name>"
+    print "./monitor.py -u <url> -m <module> -P <project_name> (-p <packages>)"
     sys.exit(3)
 
 if __name__ == "__main__":
     url = ""
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "u:p:P:")
+        opts, args = getopt.getopt(sys.argv[1:], "u:p:m:P:")
     except getopt.GetoptError:
         print "Get options Error!"
         sys.exit(2)
@@ -110,6 +129,8 @@ if __name__ == "__main__":
             url = value.strip()
         elif opt in ("-p", "--packages"):
             packages_monitoring = value.split()
+        elif opt in ("-m", "--module"):
+            module = value.strip()
         elif opt in ("-P", "--project"):
             proj_name = value.strip()
         else:
