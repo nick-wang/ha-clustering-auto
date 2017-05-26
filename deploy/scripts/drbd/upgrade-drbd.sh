a6ca8d84e713c667077a5935406450af7c80fcc5
Nick Wang <nwang@suse.com>
2017-05-26 11:05:44 +0800

    Fix drbd typo and a potenial issue.

--- deploy/scripts/drbd/upgrade-drbd.sh
+++ deploy/scripts/drbd/upgrade-drbd.sh
@@ -28,11 +28,12 @@ runOnAllNodes ${CLUSTER_CONF} "rpm -qa|sort>${LOG_DIR}/rpm-before-update"
 nextPhase "Upgrading..."
 # Can not use ${pkgs[@]}, could use ${pkgs[*]}
 # Allow verdor change for upgrade
-runOnAllNodes ${CLUSTER_CONF} "sed -i \"s/# solver.allowVendorChange = false/solver.allowVendorChange = true\"/ /etc/zypp/zypp.conf;
-                               cat /etc/zypp/zypp.conf |grep allowVendorChange"
+runOnAllNodes ${CLUSTER_CONF} "sed -i \"s/# solver.allowVendorChange = false/solver.allowVendorChange = true\"/ /etc/zypp/zypp.conf; cat /etc/zypp/zypp.conf |grep allowVendorChange"
+
+# FIXME: Ever meet installed two versions of drbd-kmp-default... need to remove first?
 runOnAllNodes ${CLUSTER_CONF} "rm -rf ${LOG_DIR}/done; zypper --non-interactive up --replacefiles ${pkgs[*]}; sleep 1; touch ${LOG_DIR}/update-drbd-done; reboot"
 
 checkAllFinish ${CLUSTER_CONF} "${LOG_DIR}/update-drbd-done"
 
-nextPhase "Finishe upgrade. After reboot."
+nextPhase "Finished upgrade. After reboot."
 runOnAllNodes ${CLUSTER_CONF} "rpm -qa|sort>${LOG_DIR}/rpm-after-update"
