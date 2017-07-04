6593c17083c6cd4a89cf92580309f1f6232f1674
Bin Liu <bliu@suse.com>
2017-07-04 09:59:40 +0800

    fix issue in stonith type

--- deploy/cts/configAndRunCts.sh
+++ deploy/cts/configAndRunCts.sh
@@ -44,30 +44,30 @@ node_list=`echo ${new_node_list//','/' '}`
 
 sle_ver=($(echo $(getSLEVersion)))
 #deal with stonith, external/libvirt, external/sbd and external/ssh so far
-if [ $stonith_type == "external/libvirt" ];
-then
-    zypper in -y libvirt pacemaker-remote
-	#disable sbd
-    case ${sle_ver[0]} in
-      12|42.1|42.2)
-        systemctl disable sbd
-        systemctl enable corosync
-        #systemctl disable pacemaker
-        systemctl stop pacemaker
-        systemctl start pacemaker
-        zypper in -y pacemaker-cts
-        ;;
-      11)
-        chkconfig sbd off
-        #chkconfig openais off
-        service openais stop
-        service openais start
-        zypper in -y libpacemaker-devel
-        ;;
-      *)
-        echo "Not support. SLE${sle_ver[0]} SP${sle_ver[1]}"
-    esac
+zypper in -y pacemaker-remote
+#disable sbd
+case ${sle_ver[0]} in
+12|42.1|42.2)
+    systemctl disable sbd
+    systemctl enable corosync
+    #systemctl disable pacemaker
+    systemctl stop pacemaker
+    systemctl start pacemaker
+    zypper in -y pacemaker-cts
+    ;;
+11)
+    chkconfig sbd off
+    #chkconfig openais off
+    service openais stop
+    service openais start
+    zypper in -y libpacemaker-devel
+    ;;
+*)
+    echo "Not support. SLE${sle_ver[0]} SP${sle_ver[1]}"
+esac
 
+if [ $stonith_type == "external/libvirt" ];
+    zypper in -y libvirt
     stonith_args="--stonith-args hypervisor_uri='qemu+tcp://$host_ip/system',hostlist='$node_list $remote_node_list'"
 elif [ $stonith_type == "external/sbd" ];
 then
