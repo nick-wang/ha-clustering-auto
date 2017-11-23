c19a25a4bc4bb559e14341615d3989d04ce7a82b
bliu <bliu@suse.com>
2017-11-23 17:43:32 +0800

    add backing store support for installation

--- deploy/scripts/installVM.py
+++ deploy/scripts/installVM.py
@@ -59,7 +59,7 @@ def installVMs(vm_list=[], res={}, devices={}, autoyast="", backfile=False):
     if backfile == False:
         installVMs_raw(vm_list, res, devices, autoyast, os_settings, disk_pattern, default_vm_instll)
     else:
-        installVms_back(vm_list, res, devices, autoyast, os_settings, disk_pattern, default_vm_instll)
+        installVms_back("../confs/template.xml",vm_list, res, devices, autoyast, os_settings, disk_pattern, default_vm_instll)
 
 def parse_vm_args(vm, devices, disk_pattern, default_vm_instll):
     vm_name = vm['name']
@@ -116,8 +116,75 @@ def run_install_cmd(os_settings, vm_name, vm, disk, res):
                             name=vm_name)
     return autoyast, parent_fd, process
     
-def installVms_back(vm_list, res, devices, autoyast, os_settings, disk_pattern, default_vm_instll):
-    pass
+def installVms_back(templatefile, vm_list, res, devices, autoyast, os_settings, disk_pattern, default_vm_instll):
+    exitcode = 0
+    processes = {}
+    vm = vm_list[0]
+    vm_name = vm['name']
+    vm, disk = parse_vm_args(vm, devices, disk_pattern, default_vm_instll)
+    vm_list[0] = vm
+    process_dict = {}
+    process_dict["autoyast"], process_dict["pipe"], process_dict['process'] = run_install_cmd(os_settings, vm_name, vm, disk, res)
+    process = process_dict["process"]
+    process.start()
+
+    process.join(MAX_VM_INSTALL_TIMEOUT)
+    os.remove(process_dict["autoyast"])
+
+    if process.exitcode is None:
+        print "process %d for installing %s timeout\n" %(process.pid, vm_name)
+        exitcode = -1
+    elif process.exitcode != 0:
+        print "process %d for installing %s returned error %d\n" %(process.pid, vm_name, process.exitcode)
+        exitcode = -2
+
+    if exitcode != 0:
+        print "the installing processes exited with %d" % (exitcode)
+        if process.is_alive():
+            print "terminate process %d with vm %s" %(process1.pid, vm1)
+            process.terminate()
+        sys.exit(exitcode)
+
+    #1.destroy the newly installed vm, and rename the base disk
+    version='0'
+    os.system("virsh destroy %s" % vm_name)
+    os.system("virsh undefine %s" % vm_name)
+    disk_name = disk.split(':')[1]
+    disk_dir = os.path.dirname(disk_name)
+    base_image = "%s/%s-base.qcow2" % (disk_dir, version)
+    os.rename(disk_name, base_image)
+    for i in range(len(vm_list)):
+        vm = vm_list[i]
+        vm, disk = parse_vm_args(vm, devices, disk_pattern, default_vm_instll)
+        disk_name = disk.split(':')[1]
+        vm_list[i] = vm
+        xmlfile = "%s/%s" % (os.path.dirname(disk_name), vm['name'])
+        #create the new image
+        print "qemu-img create -f qcow2 %s -b %s" % (disk_name, base_image)
+        os.system("qemu-img create -f qcow2 %s -b %s" % (disk_name, base_image))
+        fill_vm_xml(templatefile, xmlfile, vm['name'], vm['memory'], vm['memory'], vm['vcpus'], disk_name, 'bridge', vm['nic'])
+        os.system("virsh create %s" % xmlfile)
+    
+    
+
+def fill_vm_xml(templatefile, xmlfile, vmname, memory, cur_mem, vcpus, disk, network, interface):
+    if os.path.exists(templatefile):
+        f = open(templatefile)
+        content = f.read()
+        f.close()
+        content = content.replace("VMNAME", vmname)
+        content = content.replace("MEMORY", str(1024*memory),1)
+        content = content.replace("CUR_MEMORY", str(1024*cur_mem),1)
+        content = content.replace("VCPUS", str(vcpus))
+        content = content.replace("DISK",disk)
+        content = content.replace("NETWORK_MODE", network)
+        content = content.replace("INTERFACE", interface)
+        f = open(xmlfile, 'w')
+        f.write(content)
+        print xmlfile
+        f.close()
+        return True
+    return False
 
 def installVMs_raw(vm_list, res, devices, autoyast, os_settings, disk_pattern, default_vm_instll):
 
@@ -129,7 +196,7 @@ def installVMs_raw(vm_list, res, devices, autoyast, os_settings, disk_pattern, d
         vm, disk = parse_vm_args(vm, devices, disk_pattern, default_vm_instll)
         vm_list[i] = vm
         process = {}
-        process["pipe"], process["autoyast"], process['process'] = run_install_cmd(os_settings, vm_name, vm, disk, res)
+        process["autoyast"], process["pipe"], process['process'] = run_install_cmd(os_settings, vm_name, vm, disk, res)
         processes[vm_name] = process
         process["process"].start()
 
@@ -190,7 +257,7 @@ if __name__ == "__main__":
     mkdir_p("/var/run/vm-install/")
     os.chmod("/var/run/vm-install/", 0755)
     if len(sys.argv) > 3:
-        usage()
+       usage()
     elif len(sys.argv) == 3:
        get_config_and_install(sys.argv[1], sys.argv[2])
     elif len(sys.argv) == 2:
