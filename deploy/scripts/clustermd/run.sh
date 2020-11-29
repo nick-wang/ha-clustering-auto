98268809c0c3444f7d650c0f4899d1fd1570c1c8
Zhao Heming <heming.zhao@suse.com>
2020-11-29 21:46:44 +0800

    test - adjust speed_limit_[max|min] value for passing sle15sp3 run.sh - use correct parameter to create tar.gz file

--- deploy/scripts/clustermd/run.sh
+++ deploy/scripts/clustermd/run.sh
@@ -6,6 +6,6 @@ rm -fr /root/tt
 ./test --testdir=clustermd_tests --save-logs --logdir=/root/tt --keep-going
 
 cd /root/
-tar cvf test_result.tar.gz /root/tt/*
+tar czvf test_result.tar.gz /root/tt/*
 
 exit 0
