fe9e20bb1cdea2c01634e8be1f18254758459e36
Nick Wang <nwang@suse.com>
2021-11-23 11:20:26 +0800

    Add record file to monitor-newbuild to pass to next job
    Should configure predefined parameter and default values
    in jenkins multiple jobs
    RECORD_FILE=/var/lib/jenkins-dummy/build-change/${JOB_NAME}/${BUILD_NUMBER}/record
    
    cd ${MONITOR_LIB_DIR}
    ./monitor-newbuild.py -l ${JOB_NAME} \
                          -p "SLE-15-SP4-Full-\w+-\d+-LATEST/" \
                          -r ${RECORD_FILE}

--- jenkins-monitor/monitor-newbuild.py
+++ jenkins-monitor/monitor-newbuild.py
@@ -10,10 +10,12 @@ import argparse
 import re
 import os
 import shutil
+import glob
 
 # <img src="/icons/folder.gif" alt="[DIR]"> <a href="SLE-15-SP4-Full-Alpha-202109-LATEST/">SLE-15-SP4-Full-Alpha-202109-LATEST/</a>                             2021-09-29 19:52    -
 HTML_FORMAT = '<a href="({})">.*</a>'
 BUILD_NUM_PATTERN = ".*-Build([0-9\.]+)-.*"
+REPO_POSTFIX = "/x86_64/DVD1/"
 
 FILE_ROOT="/var/lib/jenkins-dummy/build-change/"
 
@@ -23,6 +25,8 @@ Default = {
     "Pattern" : "SLE-15-SP4-Full-\w+-\d+-LATEST/",
     "File" : "/x86_64/DVD1/media.1/media",
     "JobLocation" : "dummy/project/version/name",
+    # Record file should be like /var/lib/jenkins-dummy/build-change/${JOB_NAME}/${BUILD_NUMBER}/record
+    "Record" : "",
 }
 
 class Resource:
@@ -31,6 +35,7 @@ class Resource:
         self.url = url
         self.name = name
         self.file = file
+        self.repo = url + "/" + name + REPO_POSTFIX
         self.full = url + "/" + name + "/" + file
         self.version = 0
 
@@ -94,12 +99,28 @@ def writeNew(path, res):
     fd.writelines([res.getVersion()])
     fd.close()
 
+def recordBuild(f, res):
+    os.makedirs(os.path.dirname(f), exist_ok=True)
+
+    info = []
+    info.append("URL=%s\n" % res.repo)
+    info.append("NAME=%s\n" % res.name.strip("/"))
+
+    print("\nRecord Build name and url to %s" % f)
+    print("%s%s" % (info[0], info[1]))
+
+    fd = open(f, "w")
+    fd.writelines(info)
+    fd.close()
+
 def main():
     parser = argparse.ArgumentParser(description="script to SLE monitor build change of url pattern",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('-d', '--dry-run', dest='dry_run', action='store_true',
                         help="Only check and list the latest one.")
+    parser.add_argument('-c', '--not-clean', dest='clean', action='store_false',
+                        help="Clean the old builds record.")
     parser.add_argument('-u', '--url', metavar='url', type=str,
                         help="URL of the repo to check.", default=Default["URL"])
     parser.add_argument('-p', '--pattern', metavar='pattern', type=str,
@@ -108,6 +129,8 @@ def main():
                         help="The file of the media file in ISO.", default=Default["File"])
     parser.add_argument('-l', '--location', metavar='location', type=str,
                         help="The location to store comparison job file.", default=Default["JobLocation"])
+    parser.add_argument('-r', '--record', metavar='record', type=str,
+                        help="The record file of URL and build infor.", default=Default["Record"])
 
     args = parser.parse_args()
 
@@ -125,12 +148,24 @@ def main():
     print("The latest %s with version %s" % (latest_res, latest_res.getVersion()))
 
     path = FILE_ROOT + "/" + args.location + "/"
-    print("Record information in folder: %s" % path)
+    print("\nRecord information in folder: %s" % path)
     writeNew(path, latest_res)
+
+    if args.clean:
+        print("\nClean the old builds record in folder: %s" % path)
+
+        dirs = glob.glob(path + "[0-9]*")
+
+        for d in dirs:
+            shutil.rmtree(d)
+
+    args.record = args.record.strip('"')
 
     if not os.path.exists(path + "sle_build"):
         shutil.move(path + "sle_newbuild", path + "sle_build")
         print("First run.")
+        if len(args.record) != 0:
+            recordBuild(args.record, latest_res)
         exit(0)
     else:
         fd = open(path + "sle_build")
@@ -143,8 +178,13 @@ def main():
         else:
             shutil.move(path + "sle_newbuild", path + "sle_build")
             print("New image Build number has changed from %s to %s." % (lines[0], latest_res.getVersion()))
+            if len(args.record) != 0:
+                recordBuild(args.record, latest_res)
             exit(0)
 
 if __name__ == "__main__":
-    #./monitor-newbuild.py -l ${JOB_NAME} -p "SLE-15-SP4-Full-\w+-\d+-LATEST/"
+    #./monitor-newbuild.py -l ${JOB_NAME} -p "SLE-15-SP4-Full-\w+-\d+-LATEST/" -r "${RECORD_FILE}"
+    # Put `RECORD_FILE=/var/lib/jenkins-dummy/build-change/${JOB_NAME}/${BUILD_NUMBER}/record`
+    # In Jenkins multijob predefined parameters
+    # source the ${RECORD_FILE} will set the ENV
     main()
